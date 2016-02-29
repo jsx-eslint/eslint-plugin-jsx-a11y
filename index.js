5fb3347efad7c30f939b8fc984925ef28011f815
Ethan Cohen <etcohen@visa.com>
2016-02-29 14:36:36 -0800

    Use onBlur instead of onChange

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = {
   rules: {
     'img-uses-alt': require('./lib/rules/img-uses-alt'),
     'onClick-uses-role': require('./lib/rules/onClick-uses-role'),
-    'mouseEvents-require-keyEvents': require('./lib/rules/mouseEvents-require-keyEvents')
+    'mouseEvents-require-keyEvents': require('./lib/rules/mouseEvents-require-keyEvents'),
+    'use-onblur-not-onchange': require('./lib/rules/use-onblur-not-onchange')
   },
   configs: {
     recommended: {
---
+++ tests/lib/rules/use-onblur-not-onchange.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Enforce img tags use alt attribute.
+ * @author Ethan Cohen
+ */
+
+'use strict';
+
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/use-onblur-not-onchange');
+var RuleTester = require('eslint').RuleTester;
+
+var parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true
+  }
+};
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+
+var expectedError = {
+  message: 'onBlur must be used instead of onchange, unless absolutely necessary and it ' +
+    'causes no negative consequences for keyboard only or screen reader users.',
+  type: 'JSXOpeningElement'
+};
+
+ruleTester.run('use-onblur-not-onchange', rule, {
+  valid: [
+    {code: '<div onBlur={() => {}} />;', parserOptions: parserOptions},
+    {code: '<div />;', parserOptions: parserOptions},
+    {code: '<div onBlur={() => {}} onChange={() => {}} />;', parserOptions: parserOptions}
+  ],
+  invalid: [
+    {code: '<div onChange={() => {}} />;', errors: [expectedError], parserOptions: parserOptions},
+    {code: '<input onChange={() => {}} />', errors: [expectedError], parserOptions: parserOptions}
+  ]
+});
