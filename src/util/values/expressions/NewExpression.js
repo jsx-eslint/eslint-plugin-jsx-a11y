fb40b4b875897607940487d025988f2ad3c73da9
Ethan Cohen <ethanvcohen@gmail.com>
2016-05-06 10:51:16 -0700

    [new] - Implement NewExpression expression handler.

---
+++ src/util/values/expressions/NewExpression.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Extractor function for a NewExpression type value node.
+ * A new expression instantiates an object with `new` keyword.
+ *
+ * @returns - an empty object.
+ */
+export default function extractValueFromNewExpression() {
+  return new Object(); // eslint-disable-line
+}
