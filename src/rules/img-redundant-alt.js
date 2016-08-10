77df7181b4cd55b519e4cf78c556ae37127a5ada
Niklas Holmberg <niklas.holmberg@svt.se>
2016-08-10 09:40:26 +0200

    add custom words to img-redundant-alt

--- src/rules/img-redundant-alt.js
+++ src/rules/img-redundant-alt.js
@@ -18,10 +18,18 @@ const REDUNDANT_WORDS = [
 
 const errorMessage = 'Redundant alt attribute. Screen-readers already announce ' +
   '`img` tags as an image. You don\'t need to use the words `image`, ' +
-  '`photo,` or `picture` in the alt prop.';
+  '`photo,` or `picture` (or any specified custom words) in the alt prop.';
 
 module.exports = context => ({
   JSXOpeningElement: node => {
+    let REDUNDANT_WORDS_EXTENDED;
+
+    if (context.options[0]) {
+      REDUNDANT_WORDS_EXTENDED = REDUNDANT_WORDS.concat(context.options[0]);
+    } else {
+      REDUNDANT_WORDS_EXTENDED = REDUNDANT_WORDS;
+    }
+
     const type = elementType(node);
     if (type !== 'img') {
       return;
@@ -37,7 +45,7 @@ module.exports = context => ({
     const isVisible = isHiddenFromScreenReader(type, node.attributes) === false;
 
     if (typeof value === 'string' && isVisible) {
-      const hasRedundancy = REDUNDANT_WORDS
+      const hasRedundancy = REDUNDANT_WORDS_EXTENDED
         .some(word => Boolean(value.match(new RegExp(`(?!{)${word}(?!})`, 'gi'))));
 
       if (hasRedundancy === true) {
@@ -53,5 +61,17 @@ module.exports = context => ({
 });
 
 module.exports.schema = [
-  { type: 'object' },
+  {
+    oneOf: [
+      { type: 'string' },
+      {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+        minItems: 1,
+        uniqueItems: true,
+      },
+    ],
+  },
 ];
