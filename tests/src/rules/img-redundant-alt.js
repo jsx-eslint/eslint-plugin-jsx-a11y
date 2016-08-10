d0020334f7e837634877a1123c8ef4a69b1e631c
Niklas Holmberg <niklas.holmberg@svt.se>
2016-08-10 09:43:34 +0200

    lint

--- tests/src/rules/img-redundant-alt.js
+++ tests/src/rules/img-redundant-alt.js
@@ -28,7 +28,8 @@ const ruleTester = new RuleTester();
 
 const expectedError = {
   message: 'Redundant alt attribute. Screen-readers already announce `img` tags as an image. ' +
-    'You don\'t need to use the words `image`, `photo,` or `picture` (or any specified custom words) in the alt prop.',
+    'You don\'t need to use the words `image`, `photo,` or `picture` ' +
+    '(or any specified custom words) in the alt prop.',
   type: 'JSXOpeningElement',
 };
 
