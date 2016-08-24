9dd7773dddde9410b01b98c823ee71109526f007
Ethan Cohen <ethanc@twitter.com>
2016-08-24 19:02:16 -0400

    [new] - Implement no-static-element-interactions rule.
    Fixes #78

---
+++ tests/src/rules/no-static-element-interactions.js
@@ -0,0 +1,73 @@
+/**
+ * @fileoverview Enforce non-interactive elements have no interactive handlers.
+ * @author Ethan Cohen
+ */
+
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
+
+import rule from '../../../src/rules/no-static-element-interactions';
+import { RuleTester } from 'eslint';
+
+const parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+const errorMessage =
+  'Visible, non-interactive elements should not have mouse or keyboard event listeners';
+
+const expectedError = {
+  message: errorMessage,
+  type: 'JSXOpeningElement',
+};
+
+ruleTester.run('onclick-has-role', rule, {
+  valid: [
+    { code: '<div className="foo" />;', parserOptions },
+    { code: '<div className="foo" {...props} />;', parserOptions },
+    { code: '<div onClick={() => void 0} aria-hidden />;', parserOptions },
+    { code: '<div onClick={() => void 0} aria-hidden={true} />;', parserOptions },
+    { code: '<input type="text" onClick={() => void 0} />', parserOptions },
+    { code: '<input onClick={() => void 0} />', parserOptions },
+    { code: '<button onClick={() => void 0} className="foo" />', parserOptions },
+    { code: '<option onClick={() => void 0} className="foo" />', parserOptions },
+    { code: '<select onClick={() => void 0} className="foo" />', parserOptions },
+    { code: '<textarea onClick={() => void 0} className="foo" />', parserOptions },
+    { code: '<a tabIndex="0" onClick={() => void 0} />', parserOptions },
+    { code: '<a onClick={() => void 0} href="http://x.y.z" />', parserOptions },
+    { code: '<a onClick={() => void 0} href="http://x.y.z" tabIndex="0" />', parserOptions },
+    { code: '<input onClick={() => void 0} type="hidden" />;', parserOptions },
+    { code: '<TestComponent onClick={doFoo} />', parserOptions },
+    { code: '<Button onClick={doFoo} />', parserOptions },
+  ],
+  invalid: [
+    { code: '<div onClick={() => void 0} />;', errors: [expectedError], parserOptions },
+    {
+      code: '<div onClick={() => void 0} role={undefined} />;',
+      errors: [expectedError],
+      parserOptions,
+    },
+    { code: '<div onClick={() => void 0} {...props} />;', errors: [expectedError], parserOptions },
+    { code: '<section onClick={() => void 0} />;', errors: [expectedError], parserOptions },
+    { code: '<main onClick={() => void 0} />;', errors: [expectedError], parserOptions },
+    { code: '<article onDblClick={() => void 0} />;', errors: [expectedError], parserOptions },
+    { code: '<header onKeyDown={() => void 0} />;', errors: [expectedError], parserOptions },
+    { code: '<footer onKeyPress={() => void 0} />;', errors: [expectedError], parserOptions },
+    {
+      code: '<div onKeyUp={() => void 0} aria-hidden={false} />;',
+      errors: [expectedError],
+      parserOptions,
+    },
+    { code: '<a onClick={() => void 0} />', errors: [expectedError], parserOptions },
+  ],
+});
