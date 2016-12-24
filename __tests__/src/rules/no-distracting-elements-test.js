6c669efd0e493cdb217b04217d90c1ca58598e92
Ethan Cohen <ethanc@twitter.com>
2016-12-24 14:50:54 -0500

    Consolidate marquee and blink rules into no-distracting-elements rule.

---
+++ __tests__/src/rules/no-distracting-elements-test.js
@@ -0,0 +1,47 @@
+/**
+ * @fileoverview Enforce distracting elements are not used.
+ * @author Ethan Cohen
+ */
+
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
+
+import { RuleTester } from 'eslint';
+import rule from '../../../src/rules/no-distracting-elements';
+
+const parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+const expectedError = element => ({
+  message: `Do not use <${element}> elements as they can create visual accessibility issues and are deprecated.`,
+  type: 'JSXOpeningElement',
+});
+
+ruleTester.run('no-marquee', rule, {
+  valid: [
+    { code: '<div />;', parserOptions },
+    { code: '<Marquee />', parserOptions },
+    { code: '<div marquee />', parserOptions },
+    { code: '<Blink />', parserOptions },
+    { code: '<div blink />', parserOptions },
+  ],
+  invalid: [
+    { code: '<marquee />', errors: [expectedError('marquee')], parserOptions },
+    { code: '<marquee {...props} />', errors: [expectedError('marquee')], parserOptions },
+    { code: '<marquee lang={undefined} />', errors: [expectedError('marquee')], parserOptions },
+    { code: '<blink />', errors: [expectedError('blink')], parserOptions },
+    { code: '<blink {...props} />', errors: [expectedError('blink')], parserOptions },
+    { code: '<blink foo={undefined} />', errors: [expectedError('blink')], parserOptions },
+  ],
+});
---
+++ src/rules/no-distracting-elements.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Enforce distracting elements are not used.
+ * @author Ethan Cohen
+ */
+
+// ----------------------------------------------------------------------------
+// Rule Definition
+// ----------------------------------------------------------------------------
+
+import { elementType } from 'jsx-ast-utils';
+import { generateObjSchema, enumArraySchema } from '../util/schemas';
+
+const errorMessage = element =>
+  `Do not use <${element}> elements as they can create visual accessibility issues and are deprecated.`;
+
+
+const DEFAULT_ELEMENTS = [
+  'marquee',
+  'blink',
+];
+
+const schema = generateObjSchema({
+  elements: enumArraySchema(DEFAULT_ELEMENTS),
+});
+
+module.exports = {
+  meta: {
+    docs: {},
+    schema: [schema],
+  },
+
+  create: context => ({
+    JSXOpeningElement: (node) => {
+      const options = context.options[0] || {};
+      const elementOptions = options.elements || DEFAULT_ELEMENTS;
+      const type = elementType(node);
+      const distractingElement = elementOptions.find((element) => {
+        if (type === element) {
+          return element;
+        }
+        return false;
+      });
+
+      if (distractingElement) {
+        context.report({
+          node,
+          message: errorMessage(distractingElement),
+        });
+      }
+    },
+  }),
+};
