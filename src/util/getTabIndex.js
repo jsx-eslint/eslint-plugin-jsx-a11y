41c7623eab2321a292650f61e17ce0ccda0c3123
Jesse Renee Beach <splendidnoise@gmail.com>
2016-12-24 17:13:09 -0800

    Return undefined from getTabIndex instead of null

--- src/util/getTabIndex.js
+++ src/util/getTabIndex.js
@@ -1,28 +1,37 @@
-import { getLiteralPropValue } from 'jsx-ast-utils';
+import { getPropValue, getLiteralPropValue } from 'jsx-ast-utils';
 
 /**
  * Returns the tabIndex value.
  */
 export default function getTabIndex(tabIndex) {
   const literalValue = getLiteralPropValue(tabIndex);
-  // Only consider string and number values.
+
+  // String and number values.
   if (['string', 'number'].indexOf(typeof literalValue) > -1) {
     // Empty string will convert to zero, so check for it explicity.
     if (
       typeof literalValue === 'string'
       && literalValue.length === 0
     ) {
-      return null;
+      return undefined;
     }
     const value = Number(literalValue);
     if (Number.isNaN(value)) {
-      return null;
+      return undefined;
     }
 
     return Number.isInteger(value)
       ? value
-      : null;
+      : undefined;
   }
 
-  return null;
+  // Booleans are not valid values, return undefined.
+  if (
+    literalValue === true
+    || literalValue === false
+  ) {
+    return undefined;
+  }
+
+  return getPropValue(tabIndex);
 }
