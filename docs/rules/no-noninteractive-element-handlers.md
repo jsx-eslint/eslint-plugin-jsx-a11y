1df2216f8c1b7b764b37ddb1e33d0b6817e40aeb
Jesse Renee Beach <splendidnoise@gmail.com>
2017-02-02 15:34:53 -0800

    Add rule no-noninteractive-element-handlers

---
+++ docs/rules/no-noninteractive-element-handlers.md
@@ -0,0 +1,20 @@
+# no-noninteractive-element-handlers
+
+Write a useful explanation here!
+
+#### References
+1.
+
+## Rule details
+
+This rule takes no arguments.
+
+### Succeed
+```jsx
+<div />
+```
+
+### Fail
+```jsx
+
+```
---
+++ src/rules/no-noninteractive-element-handlers.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview $DESCRIPTION
+ * @author $AUTHOR
+ * @flow
+ */
+
+// ----------------------------------------------------------------------------
+// Rule Definition
+// ----------------------------------------------------------------------------
+
+import type { JSXOpeningElement } from 'ast-types-flow';
+import { generateObjSchema } from '../util/schemas';
+
+const errorMessage = '';
+
+const schema = generateObjSchema();
+
+module.exports = {
+  meta: {
+    docs: {},
+    schema: [schema],
+  },
+
+  create: (context: ESLintContext) => ({
+    JSXOpeningElement: (node: JSXOpeningElement) => {
+      context.report({
+        node,
+        message: errorMessage,
+      });
+    },
+  }),
+};
