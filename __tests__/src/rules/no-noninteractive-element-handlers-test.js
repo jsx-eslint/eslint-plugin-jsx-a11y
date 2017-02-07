430e98e5886c675b87fb6d81b553e38425379847
Jesse Renee Beach <splendidnoise@gmail.com>
2017-02-07 15:59:28 -0800

    All tests passing on new is* utils

--- __tests__/src/rules/no-noninteractive-element-handlers-test.js
+++ __tests__/src/rules/no-noninteractive-element-handlers-test.js
@@ -60,6 +60,7 @@ ruleTester.run('no-noninteractive-element-handlers', rule, {
     /* End all flavors of input */
     { code: '<input type="hidden" onClick={() => void 0} />' },
     { code: '<button onClick={() => void 0} className="foo" />' },
+    { code: '<menuitem onClick={() => {}} />;' },
     { code: '<option onClick={() => void 0} className="foo" />' },
     { code: '<select onClick={() => void 0} className="foo" />' },
     { code: '<textarea onClick={() => void 0} className="foo" />' },
@@ -164,7 +165,6 @@ ruleTester.run('no-noninteractive-element-handlers', rule, {
     { code: '<mark onClick={() => {}} />;', errors: [expectedError] },
     { code: '<marquee onClick={() => {}} />;', errors: [expectedError] },
     { code: '<menu onClick={() => {}} />;', errors: [expectedError] },
-    { code: '<menuitem onClick={() => {}} />;', errors: [expectedError] },
     { code: '<meta onClick={() => {}} />;', errors: [expectedError] },
     { code: '<meter onClick={() => {}} />;', errors: [expectedError] },
     { code: '<noembed onClick={() => {}} />;', errors: [expectedError] },
