d2fbea5f6a6eebff9b3ad75fe052cf3cc8cbbf2f
Michael Altamirano <michaeljaltamirano@gmail.com>
2017-06-23 19:46:14 -0700

    Update label-has-for children content handling and make default for label-has-for both id and nesting

--- src/rules/label-has-for.js
+++ src/rules/label-has-for.js
@@ -9,6 +9,7 @@
 
 import { getProp, getPropValue, elementType } from 'jsx-ast-utils';
 import { generateObjSchema, arraySchema, enumArraySchema } from '../util/schemas';
+import hasAccessibleChild from '../util/hasAccessibleChild';
 
 const errorMessage = 'Form label must have associated control';
 
@@ -24,28 +25,37 @@ const schema = {
         generateObjSchema({ every: enumArraySchema(enumValues) }, ['every']),
       ],
     },
+    allowChildren: { type: 'boolean' },
   },
 };
 
-const validateNesting = node => !!node.parent.children.find(child => child.type === 'JSXElement');
+const validateNesting = node => node.parent.children.some(child => child.type === 'JSXElement');
+
 const validateId = (node) => {
   const htmlForAttr = getProp(node.attributes, 'htmlFor');
   const htmlForValue = getPropValue(htmlForAttr);
 
   return htmlForAttr !== false && !!htmlForValue;
 };
-const validate = (node, required) => (
-  required === 'nesting' ? validateNesting(node) : validateId(node)
-);
 
-const isValid = (node, required) => {
+const validate = (node, required, allowChildren) => {
+  if (allowChildren === true) {
+    return hasAccessibleChild(node.parent);
+  }
+  if (required === 'nesting') {
+    return validateNesting(node);
+  }
+  return validateId(node);
+};
+
+const isValid = (node, required, allowChildren) => {
   if (Array.isArray(required.some)) {
-    return required.some.some(rule => validate(node, rule));
+    return required.some.some(rule => validate(node, rule, allowChildren));
   } else if (Array.isArray(required.every)) {
-    return required.every.every(rule => validate(node, rule));
+    return required.every.every(rule => validate(node, rule, allowChildren));
   }
 
-  return validate(node, required);
+  return validate(node, required, allowChildren);
 };
 
 module.exports = {
@@ -66,9 +76,10 @@ module.exports = {
         return;
       }
 
-      const required = options.required || 'id';
+      const required = options.required || { every: ['nesting', 'id'] };
+      const allowChildren = options.allowChildren || false;
 
-      if (!isValid(node, required)) {
+      if (!isValid(node, required, allowChildren)) {
         context.report({
           node,
           message: errorMessage,
