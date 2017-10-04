4953c4c31a69ae766c23517d33efdb32113d3a88
Richard Samuelson <noobtoothfairy@gmail.com>
2017-10-04 05:35:08 +0200

    added all potential falsy values

--- src/util/attributesComparator.js
+++ src/util/attributesComparator.js
@@ -32,6 +32,9 @@ function attributesComparator(
           baseAttr.value !== null &&
           baseAttr.value !== false &&
           baseAttr.value !== undefined &&
+          baseAttr.value !== '' &&
+          baseAttr.value !== 0 &&
+          baseAttr.value !== NaN &&
           Object.prototype.hasOwnProperty.call(baseAttr, "value") &&
           baseAttr.value !== getLiteralPropValue(attribute)
         ) {
