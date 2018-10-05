651366cc8c7b1fedc4fdb4bc18df7cf285234a52
Quinn Blenkinsop <quinn@qw-in.com>
2018-10-05 14:05:36 -0700

    Make aria-role case sensitive

--- src/rules/aria-role.js
+++ src/rules/aria-role.js
@@ -54,7 +54,7 @@ module.exports = {
       // value isn't in the form of a literal.
       if (value === undefined || value === null) { return; }
 
-      const normalizedValues = String(value).toLowerCase().split(' ');
+      const normalizedValues = String(value).split(' ');
       const validRoles = [...roles.keys()].filter(role => roles.get(role).abstract === false);
       const isValid = normalizedValues.every(val => validRoles.indexOf(val) > -1);
 
