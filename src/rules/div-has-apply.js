c179d25e08e2654e6ad3ad49c8a3d2e4f92e72ac
Felicia5 <Felicia5@users.noreply.github.com>
2021-03-19 23:27:31 +0000

    corrected error messages, deleted unnecessary commented code parts

--- src/rules/div-has-apply.js
+++ src/rules/div-has-apply.js
@@ -13,15 +13,11 @@ import {
   getProp,
   getPropValue,
 } from 'jsx-ast-utils';
-// import type { JSXOpeningElement } from 'ast-types-flow';
 import { generateObjSchema, arraySchema } from '../util/schemas';
 import hasAccessibleChild from '../util/hasApplyText';
 
 // const errorMessage = 'Div should not have text apply/submit. Use button native HTML element instead.';
 
-const headings = [
-  'div',
-];
 const actionVerbs = [
   'submit',
   'finish',
@@ -40,14 +36,9 @@ module.exports = {
   create: (context) => ({
     JSXOpeningElement: (node) => {
       const literalChildValue = node.parent.children.find((child) => child.type === 'Literal' || child.type === 'JSXText' || child.type === 'Unknown');
-      /*   if (literalChildValue && literalChildValue.value === 'apply') {
-           errorMessage = 'Div should not have text apply. Use button native HTML element instead.';
-         } else {
-           errorMessage = 'it is valid';
-         } */
       const options = context.options[0] || {};
       const componentOptions = options.components || [];
-      const typeCheck = headings.concat(componentOptions);
+      const typeCheck = ['div'].concat(componentOptions);
       const nodeType = elementType(node);
 
       // Only check 'div*' elements and custom types.
@@ -57,18 +48,16 @@ module.exports = {
       if (hasAccessibleChild(node.parent) === false) {
         return;
       }
-      // if (literalChildValue && literalChildValue.value.toLowerCase() !== 'apply') {
-      //   return;
       if (actionVerbs.includes(literalChildValue && literalChildValue.value.toLowerCase()) === false) {
         return;
       }
       const tabindexProp = getProp(node.attributes, 'tabIndex');
       const roleProp = getProp(node.attributes, 'role');
-      // Missing alt prop error.
+      // Missing tabindex and role prop error.
       if ((tabindexProp === undefined) || (roleProp === undefined)) {
         context.report({
           node,
-          message: 'no attributes',
+          message: 'Missing or incorrect attributes. Action verbs should have aria attributes or native HTML elements. Please define tabIndex="0" attribute and role="button" aria role in the div. Refer to the rule defined by W3C: https://w3c.github.io/aria-practices/examples/button/button.html Otherwise, the first rule of aria is to not use aria with div if it can be achieved via native HTML, like button. Refer to the Living HTML standard on MDN website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#accessibility_concerns',
         });
         return;
       }
@@ -76,14 +65,14 @@ module.exports = {
       if (tabindexValue !== '0') {
         context.report({
           node,
-          message: 'notabvalue',
+          message: 'Missing or incorrect tabIndex attribute value. Action verbs should have aria attributes or native HTML elements. Please define tabIndex="0" attribute. Refer to the rule defined by W3C: https://w3c.github.io/aria-practices/examples/button/button.html Otherwise, the first rule of aria is to not use aria with div if it can be achieved via native HTML, like button. Refer to the Living HTML standard on MDN website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#accessibility_concerns',
         });
       }
       const roleValue = getPropValue(roleProp);
       if (roleValue !== 'button') {
         context.report({
           node,
-          message: 'noRolevalue',
+          message: 'Missing or incorrect role value. Action verbs should have aria attributes or native HTML elements. Please define role="button" attribute. Refer to the rule defined by W3C: https://w3c.github.io/aria-practices/examples/button/button.html Otherwise, the first rule of aria is to not use aria with div if it can be achieved via native HTML, like button. Refer to the Living HTML standard on MDN website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#accessibility_concerns',
         });
       }
     },
