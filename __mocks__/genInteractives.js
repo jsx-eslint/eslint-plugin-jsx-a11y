453785fdf8f99c82d57b5be55d4acea95ac5e8a3
sangikhan29 <109125374+sangikhan29@users.noreply.github.com>
2023-06-27 12:54:51 -0400

    fix getInteractives as well

--- __mocks__/genInteractives.js
+++ __mocks__/genInteractives.js
@@ -140,13 +140,13 @@ const interactiveRoles = []
   )
   .filter((role) => (
     !roles.get(role).abstract
-    && roles.get(role).superClass.some((klasses) => includes(klasses, 'widget', 'window'))
+    && roles.get(role).superClass.some((klasses) => includes(klasses, 'widget') || includes(klasses, 'window'))
   ));
 
 const nonInteractiveRoles = roleNames
   .filter((role) => (
     !roles.get(role).abstract
-    && !roles.get(role).superClass.some((klasses) => includes(klasses, 'widget', 'window'))
+    && !roles.get(role).superClass.some((klasses) => includes(klasses, 'widget') || includes(klasses, 'window'))
 
     // 'toolbar' does not descend from widget, but it does support
     // aria-activedescendant, thus in practice we treat it as a widget.
