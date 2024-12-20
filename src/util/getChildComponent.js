63971b3213fdbf7ee759816556856c18777a9ff9
michael faith <michaelfaith@users.noreply.github.com>
2024-12-20 16:38:23 -0600

    feat(label-has-associated-control): add option for enforcing label's htmlFor matches control's id
    This change adds an option to the `label-has-associated-control` rule, enforcing that the label's htmlFor attribute matches the associated control's id attribute.  Previously, the only validation done on htmlFor was that it was on the label component and had text.  There was no attempt to cross-check that value against any attribute on the associated control.  Not, when the option is enabled, cases where they don't match will report.
    
    I also took the opportunity to update the error messages so that each assert type gets an error message with verbiage specific to the assertion. (not sure if this should be called out as a separate feature in the changelog?).
    
    Note: the current implementation only checks the first instance it finds of child component that matches each control component type.  It assumes that there won't be any acceptable cases where a label would have multiple inputs nested beneath it.  Let me know if that assumption doesn't hold.
    
    Closes:

---
+++ src/util/getChildComponent.js
@@ -0,0 +1,50 @@
+/**
+ * @flow
+ */
+
+import type { JSXOpeningElement, Node } from 'ast-types-flow';
+import { elementType as rawElementType } from 'jsx-ast-utils';
+import minimatch from 'minimatch';
+
+export default function getChildComponent(
+  root: Node,
+  componentName: string,
+  maxDepth: number = 1,
+  elementType: ((node: JSXOpeningElement) => string) = rawElementType,
+): Node | undefined {
+  function traverseChildren(
+    node: Node,
+    depth: number,
+  ): Node | undefined {
+    // Bail when maxDepth is exceeded.
+    if (depth > maxDepth) {
+      return undefined;
+    }
+    if (node.children) {
+      /* $FlowFixMe */
+      for (let i = 0; i < node.children.length; i += 1) {
+        /* $FlowFixMe */
+        const childNode = node.children[i];
+        // Assume an expression container renders a label. It is the best we can
+        // do in this case.
+        if (childNode.type === 'JSXExpressionContainer') {
+          return childNode;
+        }
+        // Check for components with the provided name.
+        if (
+          childNode.type === 'JSXElement'
+          && childNode.openingElement
+          && minimatch(elementType(childNode.openingElement), componentName)
+        ) {
+          return childNode;
+        }
+        const grandChild = traverseChildren(childNode, depth + 1);
+        if (grandChild) {
+          return grandChild;
+        }
+      }
+    }
+    return undefined;
+  }
+  return traverseChildren(root, 1);
+}
