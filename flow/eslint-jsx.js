0f1845a266f06691fde44e63a5f0aae1e4c91208
michael faith <michaelfaith@users.noreply.github.com>
2024-12-20 16:38:23 -0600

    feat(label-has-associated-control): add option for enforcing label's htmlFor matches control's id
    This change adds an option to the `label-has-associated-control` rule, enforcing that the label's htmlFor attribute matches the associated control's id attribute.  Previously, the only validation done on htmlFor was that it was on the label component and had text.  There was no attempt to cross-check that value against any attribute on the associated control.  Not, when the option is enabled, cases where they don't match will report.
    
    I also took the opportunity to update the error messages so that each assert type gets an error message with verbiage specific to the assertion. (not sure if this should be called out as a separate feature in the changelog?).
    
    Note: the current implementation only checks the first instance it finds of child component that matches each control component type.  It assumes that there won't be any acceptable cases where a label would have multiple inputs nested beneath it.  Let me know if that assumption doesn't hold.
    
    Closes:

--- flow/eslint-jsx.js
+++ flow/eslint-jsx.js
@@ -3,9 +3,24 @@
  */
 import type {
   JSXAttribute,
+  JSXElement,
   JSXOpeningElement,
 } from 'ast-types-flow';
 
 export type ESLintJSXAttribute = {
   parent: JSXOpeningElement
 } & JSXAttribute;
+
+type JSXOpeningFragment = {
+  type: 'JSXOpeningFragment',
+};
+
+type JSXClosingFragment = {
+  type: 'JSXClosingFragment',
+};
+
+export type JSXFragment = JSXElement & {
+  type: 'JSXFragment',
+  openingFragment: JSXOpeningFragment,
+  closingFragment: JSXClosingFragment,
+};
--- src/rules/label-has-associated-control.js
+++ src/rules/label-has-associated-control.js
@@ -10,11 +10,13 @@
 // ----------------------------------------------------------------------------
 
 import { hasProp, getProp, getPropValue } from 'jsx-ast-utils';
-import type { JSXElement } from 'ast-types-flow';
+import type { JSXElement, JSXOpeningElement } from 'ast-types-flow';
 import minimatch from 'minimatch';
 import { generateObjSchema, arraySchema } from '../util/schemas';
 import type { ESLintConfig, ESLintContext, ESLintVisitorSelectorConfig } from '../../flow/eslint';
+import getChildComponent from '../util/getChildComponent';
 import getElementType from '../util/getElementType';
+import getParentElement from '../util/getParentElement';
 import mayContainChildComponent from '../util/mayContainChildComponent';
 import mayHaveAccessibleLabel from '../util/mayHaveAccessibleLabel';
 
@@ -24,6 +26,7 @@ const errorMessages = {
   nesting: 'A form label must have an associated control as a descendant.',
   either: 'A form label must either have a valid htmlFor attribute or a control as a descendant.',
   both: 'A form label must have a valid htmlFor attribute and a control as a descendant.',
+  htmlForShouldMatchId: 'A form label must have a htmlFor attribute that matches the id of the associated control.',
 };
 
 const schema = generateObjSchema({
@@ -40,7 +43,99 @@ const schema = generateObjSchema({
     type: 'integer',
     minimum: 0,
   },
+  shouldHtmlForMatchId: {
+    description: 'If true, the htmlFor prop of the label must match the id of the associated control',
+    type: 'boolean',
+  },
 });
+
+/**
+ * Given a label node, validate that the htmlFor prop matches the id of a child
+ * component in our list of possible control components.
+ * @param node - Label node
+ * @param controlComponents - List of control components
+ */
+const validateChildHasMatchingId = (
+  node: JSXElement,
+  controlComponents: string[],
+  recursionDepth: number,
+  elementTypeFn: (node: JSXOpeningElement) => string,
+) => {
+  const htmlForAttr = getProp(node.openingElement.attributes, 'htmlFor');
+  const htmlForValue = getPropValue(htmlForAttr);
+
+  const eligibleChildren = controlComponents.map((name) => getChildComponent(node, name, recursionDepth, elementTypeFn));
+
+  const matchingChild = eligibleChildren?.find((child) => {
+    if (!child) {
+      return false;
+    }
+
+    // If this is an expression container, we can't validate the id.
+    // So we'll assume it's valid.
+    if (child.type === 'JSXExpressionContainer') {
+      return true;
+    }
+
+    const childIdAttr = getProp(child.openingElement.attributes, 'id');
+    const childIdValue = getPropValue(childIdAttr);
+
+    return htmlForValue === childIdValue;
+  });
+  return !!matchingChild;
+};
+
+/**
+ * Given a label node, validate that the htmlFor prop matches the id of a sibling
+ * component in our list of possible control components.
+ * @param node - Label node
+ * @param controlComponents - List of control components
+ */
+const validateSiblingHasMatchingId = (
+  node: JSXElement,
+  controlComponents: string[],
+  elementTypeFn: (node: JSXOpeningElement) => string,
+  context: ESLintContext,
+) => {
+  const htmlForAttr = getProp(node.openingElement.attributes, 'htmlFor');
+  const htmlForValue = getPropValue(htmlForAttr);
+
+  const parent = getParentElement(node, context);
+
+  // Filter siblings to only include JSXExpressionContainers and JSXElements that match our list of
+  // control components.
+  const siblingElements = parent?.children.filter(
+    (child) => child !== node
+      && (child.type === 'JSXExpressionContainer'
+        || (child.type === 'JSXElement'
+          && child.openingElement
+          && controlComponents.some((name) => minimatch(elementTypeFn(child.openingElement), name)))),
+  );
+
+  if (!siblingElements || siblingElements.length === 0) {
+    return false;
+  }
+
+  // Of those siblings, find the one that has an id that matches the htmlFor prop.
+  const matchingSibling = siblingElements.find((sibling) => {
+    // We can't really do better than just assuming an expression container is valid.
+    if (sibling.type === 'JSXExpressionContainer') {
+      return true;
+    }
+    // This is only necessary because the filter operation above doesn't properly type narrow based
+    // on the filter condition.
+    if (sibling.type !== 'JSXElement') {
+      return false;
+    }
+
+    const siblingIdAttr = getProp(sibling.openingElement.attributes, 'id');
+    const siblingIdValue = getPropValue(siblingIdAttr);
+
+    return htmlForValue === siblingIdValue;
+  });
+
+  return !!matchingSibling;
+};
 
 const validateHtmlFor = (node, context) => {
   const { settings } = context;
@@ -74,6 +169,7 @@ export default ({
     const assertType = options.assert || 'either';
     const labelComponentNames = ['label'].concat(labelComponents);
     const elementType = getElementType(context);
+    const shouldHtmlForMatchId = !!options.shouldHtmlForMatchId;
 
     const rule = (node: JSXElement) => {
       const isLabelComponent = labelComponentNames.some((name) => minimatch(elementType(node.openingElement), name));
@@ -97,12 +193,7 @@ export default ({
       );
       const hasHtmlFor = validateHtmlFor(node.openingElement, context);
       // Check for multiple control components.
-      const hasNestedControl = controlComponents.some((name) => mayContainChildComponent(
-        node,
-        name,
-        recursionDepth,
-        elementType,
-      ));
+      const hasNestedControl = controlComponents.some((name) => mayContainChildComponent(node, name, recursionDepth, elementType));
       const hasAccessibleLabel = mayHaveAccessibleLabel(
         node,
         recursionDepth,
@@ -126,6 +217,7 @@ export default ({
               node: node.openingElement,
               message: errorMessages.htmlFor,
             });
+            return;
           }
           break;
         case 'nesting':
@@ -134,6 +226,7 @@ export default ({
               node: node.openingElement,
               message: errorMessages.nesting,
             });
+            return;
           }
           break;
         case 'both':
@@ -142,6 +235,7 @@ export default ({
               node: node.openingElement,
               message: errorMessages.both,
             });
+            return;
           }
           break;
         case 'either':
@@ -150,10 +244,20 @@ export default ({
               node: node.openingElement,
               message: errorMessages.either,
             });
+            return;
           }
           break;
         default:
           break;
+      }
+      // Lastly, let's check to see if the htmlFor prop matches the id of a valid sibling or descendent component.
+      if (shouldHtmlForMatchId && hasHtmlFor) {
+        if (!validateSiblingHasMatchingId(node, controlComponents, elementType, context) && !validateChildHasMatchingId(node, controlComponents, recursionDepth, elementType)) {
+          context.report({
+            node: node.openingElement,
+            message: errorMessages.htmlForShouldMatchId,
+          });
+        }
       }
     };
 
